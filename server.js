8ac27dfa1dfa3584f214426ca0ea9606740517fd
Nur Hossain Hemel <90263811+Nur-Hossain-Memel-99@users.noreply.github.com>
2022-05-28 17:11:01 +0600

    update 1

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+let initial_path = path.join(__dirname,"public");
+
+const app = express();
+app.use(express.static(initial_path));
+
+app.get('/',(req,res) => {
+    res.sendFile(path.join(initial_path,"home.html"));
+})
+
+app.listen("3000",() => {
+    console.log('listeng...PORT 3000  http://localhost:3000');
+})
